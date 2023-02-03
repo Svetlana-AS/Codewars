06cd8636911126e8f3d350447f18e490e3812033
Svetlana-AS <98954625+Svetlana-AS@users.noreply.github.com>
2023-02-03 10:03:34 +0100

    Type of sum

--- function spongeMeme.js
+++ function spongeMeme.js
@@ -11,3 +11,12 @@ function spongeMeme(sentence) {
 }
 
 console.log(sentence("stop Making spongebob Memes!"))
+
+
+
+// function getRectangleString(w, h) {
+//     const rn = "\r\n";
+//     const tb = '*'.repeat(w) + rn;
+//     const center = (w > 1) ? ('*' + ' '.repeat(w - 2) + '*' + rn).repeat(h - 2) : '';
+//     return h > 1 ? (tb + center + tb) : tb.repeat(h);
+// }
