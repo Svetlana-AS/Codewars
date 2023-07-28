25f10bc721a06ecf2a68c7f24246bc6ee57f0ab3
Svetlana-AS <astaninasv1@gmail.com>
2023-07-28 14:38:40 +0200

    Basic Mathematical Operations

---
+++ BasicMathematicalOperations.js
@@ -0,0 +1,26 @@
+// Basic Mathematical Operations
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+function basicOp(operation, value1, value2)
+{
+
+    switch (operation){
+        case '+':
+            return value1+value2;
+        case '-' :
+            return value1-value2;
+        case '*':
+            return value1*value2;
+        case '/':
+            return value1/value2;
+    }
+
+}
+
+function basicOp(operation, value1, value2)
+{
+    return eval(value1 + operation + value2);
+}
